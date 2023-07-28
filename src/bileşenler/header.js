6d491a73ff7e5e953f25269d1e75c2889388d0f6
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-07-28 17:40:12 +0300

    header: Task 2 is completed: Function headerEkleyici is created

--- src/bileşenler/header.js
+++ src/bileşenler/header.js
@@ -11,7 +11,25 @@ const Header = (baslik, tarih, yazi) => {
   //    <span class="temp">{ yazi }</span>
   //  </div>
   //
-}
+
+  const headerDiv = document.createElement("div");
+  headerDiv.className = "header";
+
+  const dateSpan = document.createElement("span");
+  dateSpan.className = "date";
+  dateSpan.textContent = tarih;
+
+  const baslikH1 = document.createElement("h1");
+  baslikH1.textContent = baslik;
+
+  const tempSpan = document.createElement("span");
+  tempSpan.className = "temp";
+  tempSpan.textContent = yazi;
+
+  headerDiv.append(dateSpan, baslikH1, tempSpan);
+
+  return headerDiv;
+};
 
 const headerEkleyici = (secici) => {
   // GÖREV 2
@@ -20,10 +38,18 @@ const headerEkleyici = (secici) => {
   // Görev 1'de tanımladığınız Header bileşenini kullanarak bir header oluşturun (baslik,tarih,yazi parametrelerini kendi isteğinize göre belirleyin)
   // Oluşturulan header'i, verilen seçiciyle eşleşen DOM'daki öğeye eklemelidir.
   //
-
-  // İPUCU: querySelector bir string alabilir (bknz: querySelector("#wrapper")) 
+  // İPUCU: querySelector bir string alabilir (bknz: querySelector("#wrapper"))
   // fakat aynı zamanda bir değişken de alabilir (bknz: querySelector(secici))
- 
-}
 
-export { Header, headerEkleyici }
+  const headerDOM = Header(
+    "Barbie'nin film müzikleri bir albümde toplandı",
+    "26.07.2023",
+    "Greta Gerwig'in yönetmen koltuğunda oturduğu Barbie filmi şu sıralar tüm dünyanın gündeminde. Barbie gibi giyinip sinemaya gidenler, sosyal medyada 'Hi Barbie!' akımına ayak uyduranlar veya Barbie film müziklerini durmadan dinleyenler... Dua Lipa, Nicki Minaj ve KAROL G gibi birçok ünlü müzisyenin şarkıları, 'Barbie The Movie' albümünde yer aldı."
+  );
+
+  const contDOM = document.querySelector(secici);
+
+  contDOM.append(headerDOM);
+};
+
+export { Header, headerEkleyici };
